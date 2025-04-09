2e9333887b0f1faeca7998982fd47609406958af
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-04-08 21:08:36 -0700

    brainstorm features and components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,32 @@ function App() {
   return (
     <>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+
+{/* SIDE BAR */}
+<h3>Your Projects</h3>
+<button>+ Add Project</button>
+  {/* LISTS CREATED PROJECTS - CLICK ON PROJECT OPENS DETAIL PAGE */}
+
+      {/* FORM */}
+      <button>Cancel</button><button>Save</button>
+        {/* SAVE ADDS PROJECT TO PROJECT SIDEBAR */}
+
+      {/* TITLE PROJECT */}
+      {/* DESCRIPTION */}
+      {/* DUE DATE */}
+
+
+      {/* PROJECT DETAIL PAGE*/}
+        {/* DELETE PROJECT BUTTON */}
+        {/* TITLE ENTERED*/}
+        {/* DATE ENTERED */}
+        {/* DESCRIPTION ENTERED */}
+      {/* TASKS FOR PROJECT */}
+        {/* ADD TASK FIELD */}
+        {/* ADD TASK BUTTON */}
+        {/* LIST OF ADDED TASKS */}
+        {/* CLEAR TASK BUTTON */}
+
     </>
   );
 }
