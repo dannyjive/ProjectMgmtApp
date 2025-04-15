37d47bbd83866e29cce7c5087d67c695595b7078
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-04-15 09:41:18 -0700

    added useref for forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 import { useState } from "react";
 
 function App() {
 
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
 
+  function handleStartAddProject(){
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData){
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject ]
+      }
+    })
+  }
  
+  let content;
+
+  console.log(projectState)
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />
+  } else if (projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
 
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
   
-        <ProjectSidebar/>
-        <NewProject />
+        <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+        {content}
         
 
       </main>
